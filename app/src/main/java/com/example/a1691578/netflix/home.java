26d7dd43b1805637e3691ae02bc63a2f3834a2c6
1691544 <shainaarora1234@gmail.com>
2017-05-14 16:51:49 -0400

    grewal

--- app/src/main/java/com/example/a1691578/netflix/home.java
+++ app/src/main/java/com/example/a1691578/netflix/home.java
@@ -1,13 +1,20 @@
 package com.example.a1691578.netflix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
     }
+    public void mainClick(View view) {
+        Intent intent = new Intent(this, Sidebar.class);
+        startActivity(intent);
+    }
+
 }
