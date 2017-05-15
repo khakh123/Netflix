1bce348f5343a0a8a586dff29a7dc97734f6241e
khakh123 <manjotkhakh@gmail.com>
2017-05-15 08:49:56 -0400

    manjot4

--- app/src/main/java/com/example/a1691578/netflix/Sidebar.java
+++ app/src/main/java/com/example/a1691578/netflix/Sidebar.java
@@ -2,12 +2,18 @@ package com.example.a1691578.netflix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
 public class Sidebar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sidebar);
+    }
+
+    public void manjot(View view) {
+        Intent intent = new Intent(this, Users.class);
+        startActivity(intent);
     }
 }
